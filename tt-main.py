539cf19a3f7652e78da947caf05ce571ba283a56
trbouma@gmail.com <trbouma@gmail.com>
2021-03-17 18:43:05 -0400

    added check write status again

--- tt-main.py
+++ tt-main.py
@@ -217,6 +217,11 @@ print("Starting up! Version 2021-03-16 heroku-20 stack " + start_time.strftime("
 print('Journal Time:', os.environ['JOURNAL_TIME'])
 
 print(check_journal_write_status())
+if check_journal_write_status():
+    Print('Journal is writing')
+else:
+    Print('Journal is done')
+
 print(random_tweet())
 twitter_update(random_tweet())
 send_sketch()
