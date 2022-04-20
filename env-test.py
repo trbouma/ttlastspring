d49003e1c32de8e2c824382d41e352683b033248
trbouma <trbouma@gmail.com>
2022-04-20 16:53:27 -0400

    local env

--- env-test.py
+++ env-test.py
@@ -1,2 +1,16 @@
 import os
+from dotenv import load_dotenv
+import psycopg2
+
+def test_connect():
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM journal_entries WHERE id=213 """)
+    query_results = cur.fetchall()
+    print(query_results)
+
+load_dotenv()
+
 print(os.environ['TIM'])
+print(os.environ['DATABASE_URL'])
+test_connect()
