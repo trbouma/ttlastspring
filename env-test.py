000efed8e5dfcc3cf6f81b90963e181b8bae1199
trbouma <trbouma@gmail.com>
2022-04-21 17:07:08 -0400

    more changes

--- env-test.py
+++ env-test.py
@@ -9,8 +9,28 @@ def test_connect():
     query_results = cur.fetchall()
     print(query_results)
 
+def test_row_fetch():
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    cur.execute("""SELECT * FROM journal_entries WHERE journal_date like '2020 01 05%' """)
+    query_results = cur.fetchall()
+
+    for one_row in query_results:
+        print(one_row[3])
+
+def test_catalogue_fetch():
+    conn = psycopg2.connect(os.environ['DATABASE_URL'])
+    cur = conn.cursor()
+    cur.execute("""SELECT count(*) FROM catalogue """)
+    query_results = cur.fetchall()
+
+    for one_row in query_results:
+        print(one_row)
+
 load_dotenv()
 
 print(os.environ['TIM'])
 print(os.environ['DATABASE_URL'])
-test_connect()
+# test_connect()
+# test_row_fetch()
+test_catalogue_fetch()
